981a4e5ca9e187d44cf272791bf87b4874e10d27
CoderFek <praveen.anand2212@gmail.com>
2025-07-01 15:55:08 +0530

    frontend

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from api.routes import subscribe, send
 from api.db import init_db
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# cors settings
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(subscribe.router, prefix="")
 app.include_router(send.router, prefix="")
